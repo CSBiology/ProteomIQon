4dccd7848364c291a154969a78fe39ab643edf1e
ZimmerD <davidzimmer91@gmail.com>
2019-05-06 14:18:39 +0200

    fix createcentroidizationParams.fsx

--- src/ProteomIQon/Scripts/createCentroidizationParams.fsx
+++ src/ProteomIQon/Scripts/createCentroidizationParams.fsx
@@ -5,52 +5,52 @@
 #r @"../../../packages\FSharpAux.IO\lib\netstandard2.0\FSharpAux.IO.dll"
 
 open ProteomIQon
+open ProteomIQon.Dto
 open ProteomIQon.Domain
 
-//open ProteomIQon.Library
 
-//let defaultCentroidizationParams = 
+let defaultCentroidizationParams = 
 
-//    let ms1CentroidizationParams = 
-//        {
-//            NumberOfScales        = 3
-//            YThreshold            = 1. 
-//            MzTolerance           = 0.1
-//            SNRS_Percentile       = 95.
-//            MinSNR                = 1.        
-//        }
+    let ms1CentroidizationParams  = 
+        {
+            NumberOfScales        = 3
+            YThreshold            = 1.
+            MzTolerance           = 0.1
+            SNRS_Percentile       = 95.
+            MinSNR                = 1.        
+        }
     
-//    let ms2CentroidizationParams = 
-//        {
-//            /// Centroidization
-//            NumberOfScales          = 10
-//            Centroid_MzTolerance    = 0.1
-//            SNRS_Percentile         = 90.
-//            MinSNR                  = 1. 
-//            /// Padding
-//            MaximumPaddingPoints    = Some 7
-//            Padding_MzTolerance     = 0.05
-//            WindowSize              = 150
-//            SpacingPerc             = 95. 
-//        } 
+    let ms2CentroidizationParams = 
+        {
+            /// Centroidization
+            NumberOfScales          = 10
+            Centroid_MzTolerance    = 0.1
+            SNRS_Percentile         = 95.
+            MinSNR                  = 1.
+            /// Padding
+            MaximumPaddingPoints    = Some 7
+            Padding_MzTolerance     = 0.05
+            WindowSize              = 150
+            SpacingPerc             = 95.
+        } 
          
-//    {
-//        Centroid                    = true
-//        UseManufacturerCentroids    = false
-//        StartRetentionTime          = nan
-//        EndRetentionTime            = nan
-//        MS1Centroidization          = ms1CentroidizationParams
-//        MS2CentroidizationParams    = ms2CentroidizationParams
-//    }
+    {
+        Centroid                    = true
+        UseManufacturerCentroids    = false
+        StartRetentionTime          = nan
+        EndRetentionTime            = nan
+        MS1Centroidization          = ms1CentroidizationParams
+        MS2CentroidizationParams    = ms2CentroidizationParams
+    }
 
 
-//let serialized = 
-//    defaultCentroidizationParams
-//    |> Dto.CentroidizationParams.fromDomain
-//    |> Json.serialize
+let serialized = 
+    defaultCentroidizationParams
+    |> Json.serialize
 
-//System.IO.File.WriteAllText( @"C:\Users\david\Source\Repos\netCoreRepos\ProteomIQon\src\ProteomIQon\defaultParams\centroidizationParams.json",serialized)
+System.IO.File.WriteAllText( @"C:\Users\david\Source\Repos\netCoreRepos\ProteomIQon\src\ProteomIQon\defaultParams\centroidizationParams.json",serialized)
 
-//let deserialized = 
-//    System.IO.File.ReadAllText(@"C:\Users\david\Source\Repos\netCoreRepos\ProteomIQon\src\ProteomIQon\defaultParams\centroidizationParams.json")
-//    |> Json.deserialize<CentroidizationParams>
+let deserialized = 
+    System.IO.File.ReadAllText(@"C:\Users\david\Source\Repos\netCoreRepos\ProteomIQon\src\ProteomIQon\defaultParams\centroidizationParams.json")
+    |> Json.deserialize<Dto.CentroidizationParams>
+    |> CentroidizationParams.toDomain
