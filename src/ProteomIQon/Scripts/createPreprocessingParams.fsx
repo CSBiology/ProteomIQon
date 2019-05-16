78f9d4f49ba01e995ae4bc9c217718aa3b67c146
ZimmerD <davidzimmer91@gmail.com>
2019-05-16 16:45:59 +0200

    refactor param creation script

---
+++ src/ProteomIQon/Scripts/createPreprocessingParams.fsx
@@ -0,0 +1,62 @@
+// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
+// for more guidance on F# programming.
+#r "netstandard"
+#r @"../../../bin\ProteomIQon\netstandard2.0\ProteomIQon.dll"
+#r @"../../../packages\BioFSharp.Mz\lib\netstandard2.0\BioFSharp.Mz.dll"
+#r @"../../../packages\FSharpAux.IO\lib\netstandard2.0\FSharpAux.IO.dll"
+
+open ProteomIQon
+open ProteomIQon.Dto
+open ProteomIQon.Domain
+System.IO.Path.GetExtension "C:\Users\david\Source\Repos\netCoreRepos\ProteomIQon\src\ProteomIQon\defaultParams\20190208_TT_FS_SA_MS1041_SFbdgGel001.wiff"
+
+let defaultPreprocessingParams = 
+
+    let ms1PeakPickingParams  = 
+        {
+            
+            NumberOfScales          = 3
+            YThreshold              = YThreshold.Fixed 1.
+            Centroid_MzTolerance    = 0.1
+            SNRS_Percentile         = 95.
+            MinSNR                  = 1.
+            PaddingParams           = None
+        }
+
+    let ms2PaddingParams = 
+        {
+            MaximumPaddingPoints    = Some 7
+            Padding_MzTolerance     = 0.05
+            WindowSize              = 150
+            SpacingPerc             = 95.
+        }
+    let ms2PeakPickingParams = 
+        {
+   
+            NumberOfScales          = 10
+            YThreshold              = YThreshold.MinSpectrumIntensity
+            Centroid_MzTolerance    = 0.1
+            SNRS_Percentile         = 95.
+            MinSNR                  = 1.
+            PaddingParams           = Some ms2PaddingParams
+        } 
+         
+    {
+        Compress                    = true
+        StartRetentionTime          = None
+        EndRetentionTime            = None 
+        MS1PeakPicking              = PeakPicking.Centroid (CentroidizationMode.Wavelet ms1PeakPickingParams)
+        MS2PeakPicking              = PeakPicking.Centroid (CentroidizationMode.Wavelet ms2PeakPickingParams)
+    }
+
+
+let serialized = 
+    defaultPreprocessingParams
+    |> Json.serialize
+
+System.IO.File.WriteAllText( @"C:\Users\david\Source\Repos\netCoreRepos\ProteomIQon\src\ProteomIQon\defaultParams\preprocessingParams.json",serialized)
+
+let deserialized = 
+    System.IO.File.ReadAllText(@"C:\Users\david\Source\Repos\netCoreRepos\ProteomIQon\src\ProteomIQon\defaultParams\preprocessingParams.json")
+    |> Json.deserialize<Dto.PreprocessingParams>
+    |> PreprocessingParams.toDomain
