6962243a4baefbdc5dfdae2eea5e0534d0946d35
caroott <caroline.ott1994@gmail.com>
2019-09-04 13:28:05 +0200

    Add template for PSMStatisticsXtandem

---
+++ src/PSMStatisticsXtandem/Program.fs
@@ -0,0 +1,19 @@
+namespace ProteomIQon
+
+open System
+open CLIArgumentParsing
+open Argu
+module console1 =
+
+    [<EntryPoint>]
+    let main argv = 
+        printfn "%A" argv
+
+        let parser = ArgumentParser.Create<CLIArguments>(programName =  (System.Reflection.Assembly.GetExecutingAssembly().GetName().Name)) 
+        let usage  = parser.PrintUsage()
+        printfn "%s" usage
+        let results = parser.Parse argv
+        Library.printParams (results.GetAllResults())
+        printfn "Hit any key to exit."
+        System.Console.ReadKey() |> ignore
+        0
