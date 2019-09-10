a07b2ba9db6e4b429f34f508da327706acaf7ec0
caroott <caroline.ott1994@gmail.com>
2019-09-10 10:36:51 +0200

    Add logging to PeptideDB

--- src/PeptideDB/Program.fs
+++ src/PeptideDB/Program.fs
@@ -7,22 +7,24 @@ open Argu
 module console1 =
 
     [<EntryPoint>]
-    let main argv = 
+    let main argv =
         printfn "%A" argv
 
-        let parser = ArgumentParser.Create<CLIArguments>(programName =  (System.Reflection.Assembly.GetExecutingAssembly().GetName().Name)) 
+        let parser = ArgumentParser.Create<CLIArguments>(programName =  (System.Reflection.Assembly.GetExecutingAssembly().GetName().Name))
         let results = parser.Parse argv
         let outputDir = results.TryGetResult OutputDirectory
         let paramF = results.TryGetResult ParamFile
-        match outputDir, paramF with 
-        | Some o , Some p -> 
-            printfn "Output directory -o = %s" o
-            printfn "Parameter file path -p = %s" p
+        match outputDir, paramF with
+        | Some o , Some p ->
+            let logger = Logging.createLogger (sprintf @"%s\db_log.txt" o) "PeptideDB"
+            logger.Trace (sprintf "CLIArguments %A" results)
+            logger.Info (sprintf "OutputFilePath %s" o)
+            logger.Info (sprintf "ParamFilePath %s" p)
             Directory.CreateDirectory(o) |> ignore
-            let processParams = 
+            let processParams =
                 Json.ReadAndDeserialize<Dto.PeptideDBParams> p
                 |> Dto.PeptideDBParams.toDomain
-            PeptideDB.createPeptideDB processParams o 
+            PeptideDB.createPeptideDB processParams o
         | _ -> failwith "params are not guut"
         printfn "Hit any key to exit."
         System.Console.ReadKey() |> ignore
