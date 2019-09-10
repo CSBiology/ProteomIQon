b8ce287e3bee4ac87f7d302430c3a0ca886809ce
Jonathan Ott <jonathan.ott1994@gmail.com>
2019-09-10 11:15:23 +0200

    Add logging to Preprocessing

--- src/Preprocessing/Program.fs
+++ src/Preprocessing/Program.fs
@@ -7,63 +7,64 @@ open Argu
 module console1 =
 
     [<EntryPoint>]
-    let main argv = 
+    let main argv =
         printfn "%A" argv
 
-        let parser = ArgumentParser.Create<CLIArguments>(programName =  (System.Reflection.Assembly.GetExecutingAssembly().GetName().Name)) 
+        let parser = ArgumentParser.Create<CLIArguments>(programName =  (System.Reflection.Assembly.GetExecutingAssembly().GetName().Name))
         let results = parser.Parse argv
         let i = results.GetResult InstrumentOutput
         let o = results.GetResult OutputDirectory
-        let p = results.GetResult ParamFile     
-        printfn "InputFilePath -i = %s" i
-        printfn "InputFilePath -o = %s" o
-        printfn "InputFilePath -p = %s" p
+        let p = results.GetResult ParamFile
         Directory.CreateDirectory(o) |> ignore
-        let processParams = 
+        let logger = Logging.createLogger (sprintf @"%s\run_log.txt" o) "Preprocessing"
+        logger.Info (sprintf "InputFilePath -i = %s" i)
+        logger.Info (sprintf "OutputFilePath -o = %s" o)
+        logger.Info (sprintf "ParamFilePath -p = %s" p)
+        logger.Trace (sprintf "CLIArguments: %A" results)
+        let processParams =
                 Json.ReadAndDeserialize<Dto.PreprocessingParams> p
-                |> Dto.PreprocessingParams.toDomain  
-        
+                |> Dto.PreprocessingParams.toDomain
+
         if File.Exists i || (Directory.Exists i && i.EndsWith(".d"))  then
-            printfn "singleFile"
+            logger.Info "single file"
             Preprocessing.processFile processParams o i
-        elif Directory.Exists i then 
-            printfn "multiple files"
-            let files = 
+            logger.Trace (sprintf "Preprocessing %s" i)
+        elif Directory.Exists i then
+            logger.Info "multiple files"
+            let files =
                 Core.MzLite.Reader.getMSFilePaths i
                 |> Array.sort
-            files 
-            |> Array.iter (printfn "%s")
-            let c = 
-                match results.TryGetResult Parallelism_Level with 
+            logger.Trace (sprintf "Preprocessing multiple files: %A" files)
+            let c =
+                match results.TryGetResult Parallelism_Level with
                 | Some c    -> c
                 | None      -> 1
-            try  
-            let partitionedFiles = 
+            logger.Trace (sprintf "Program is running on %i cores" c)
+            try
+            let partitionedFiles =
                 files
-                |> Array.splitInto c           
+                |> Array.splitInto c
             [for i in partitionedFiles do yield async { return i |> Array.map (Preprocessing.processFile processParams o)}]
             |> Async.Parallel
             |> Async.RunSynchronously
             |> ignore
 
-            //files 
-            //|> FSharpAux.PSeq.map (fun inp -> 
+            //files
+            //|> FSharpAux.PSeq.map (fun inp ->
             //                        try
             //                            Preprocessing.processFile processParams o inp
             //                        with
-            //                        | ex -> 
-            //                            printfn "%A" ex 
-                                        
-            //                      ) 
+            //                        | ex ->
+            //                            printfn "%A" ex
+
+            //                      )
             //|> FSharpAux.PSeq.withDegreeOfParallelism c
             //|> Array.ofSeq
             //|> ignore
-            with 
+            with
             | ex -> printfn "%A" ex
-        else 
+        else
             failwith "The given path to the instrument output is neither a valid file path nor a valid directory path."
-        printfn "Hit any key to exit."
+        logger.Info "Hit any key to exit."
         System.Console.ReadKey() |> ignore
         0
-        
-
