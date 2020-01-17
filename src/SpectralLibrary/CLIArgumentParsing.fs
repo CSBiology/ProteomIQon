6b53a18e8a4c2eb4ec6215195371025f5dc9aa26
caroott <caroline.ott1994@gmail.com>
2020-01-17 15:38:11 +0100

    Add functionality for console tool

--- src/SpectralLibrary/CLIArgumentParsing.fs
+++ src/SpectralLibrary/CLIArgumentParsing.fs
@@ -6,17 +6,20 @@ module CLIArgumentParsing =
     open System.IO
   
     type CLIArguments =
-        | [<AltCommandLine("-i")>] InstrumentOutput of path:string
-        | [<AltCommandLine("-o")>] OutputDirectory  of path:string 
-        | [<AltCommandLine("-p")>] ParamFile of path:string
-        | [<AltCommandLine("-l")>] Log_Level of level:int
-        | [<AltCommandLine("-v")>] Verbosity_Level of level:int
+        | [<Mandatory>] [<AltCommandLine("-i")>] InstrumentOutput    of path:string
+        | [<Mandatory>] [<AltCommandLine("-o")>] OutputDirectory     of path:string
+        | [<Mandatory>] [<AltCommandLine("-p")>] ParamFile           of path:string
+        | [<Mandatory>] [<AltCommandLine("-d")>] PeptideDataBase     of path:string
+        | [<Mandatory>] [<AltCommandLine("-q")>] PSMStatisticsResult of path:string
+        | [<Unique>]    [<AltCommandLine("-c")>] Parallelism_Level of level:int
+
     with
         interface IArgParserTemplate with
             member s.Usage =
                 match s with
-                | InstrumentOutput _ -> "specify mass spectrometry Output"
-                | OutputDirectory  _ -> "specify output directory"
-                | ParamFile _        -> "specify param file For centroidization"
-                | Log_Level _        -> "set the log level."
-                | Verbosity_Level _  -> "set the verbosity level."
+                | InstrumentOutput _    -> "Specify mass spectrometry Output"
+                | OutputDirectory  _    -> "Specify output directory"
+                | ParamFile _           -> "Specify param file For centroidization"
+                | PeptideDataBase _     -> "Specify the file path of the peptide data base."
+                | PSMStatisticsResult _ -> "Specify the qpsm file/folder"
+                | Parallelism_Level _   -> "Set the number of cores the programm can use. Parallelization occurs on file level. This flag is only of effect if a input directory (-i) is specified."
