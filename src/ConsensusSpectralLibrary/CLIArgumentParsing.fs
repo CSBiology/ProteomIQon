b1618293019a655379604112f8620ca782362889
caroott <caroline.ott1994@gmail.com>
2020-02-06 09:40:39 +0100

    Add console functions

--- src/ConsensusSpectralLibrary/CLIArgumentParsing.fs
+++ src/ConsensusSpectralLibrary/CLIArgumentParsing.fs
@@ -6,17 +6,14 @@ module CLIArgumentParsing =
     open System.IO
   
     type CLIArguments =
-        | [<AltCommandLine("-i")>] InstrumentOutput of path:string
+        | [<AltCommandLine("-i")>] SpectralLibrary of path:string
         | [<AltCommandLine("-o")>] OutputDirectory  of path:string 
         | [<AltCommandLine("-p")>] ParamFile of path:string
-        | [<AltCommandLine("-l")>] Log_Level of level:int
-        | [<AltCommandLine("-v")>] Verbosity_Level of level:int
+
     with
         interface IArgParserTemplate with
             member s.Usage =
                 match s with
-                | InstrumentOutput _ -> "specify mass spectrometry Output"
+                | SpectralLibrary _  -> "specify spectral libraries"
                 | OutputDirectory  _ -> "specify output directory"
-                | ParamFile _        -> "specify param file For centroidization"
-                | Log_Level _        -> "set the log level."
-                | Verbosity_Level _  -> "set the verbosity level."
+                | ParamFile _        -> "specify param file for consensus library generation"
