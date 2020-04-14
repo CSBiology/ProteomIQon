4b5c4e412fff3e83e88aac9353c41b484a2e3703
ZimmerD <davidzimmer91@gmail.com>
2020-04-14 13:09:03 +0200

    add Thermo preprocessing paramfile

--- src/ProteomIQon/Scripts/createPreprocessingParams.fsx
+++ src/ProteomIQon/Scripts/createPreprocessingParams.fsx
@@ -44,9 +44,9 @@ let defaultPreprocessingParams :Dto.PreprocessingParams =
         } 
          
     {
-        Compress                    = MzIO.Binary.BinaryDataCompressionType.NumPressZLib
+        Compress                    = MzIO.Binary.BinaryDataCompressionType.NoCompression
         StartRetentionTime          = None
-        EndRetentionTime            = None 
+        EndRetentionTime            = None
         MS1PeakPicking              = PeakPicking.Centroid (CentroidizationMode.Wavelet ms1PeakPickingParams)
         MS2PeakPicking              = PeakPicking.Centroid (CentroidizationMode.Wavelet ms2PeakPickingParams)
     }
