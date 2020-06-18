c3e0c7c972bc571baa638db597fae50870498671
Jonathan Ott <39764934+Joott@users.noreply.github.com>
2020-06-18 17:00:26 +0200

    Add program logic

--- src/TableSort/Program.fs
+++ src/TableSort/Program.fs
@@ -1,8 +1,10 @@
 namespace ProteomIQon
 
-open System
+open System.IO
 open CLIArgumentParsing
 open Argu
+open TableSort
+
 module console1 =
 
     [<EntryPoint>]
@@ -11,6 +13,43 @@ module console1 =
 
         let parser = ArgumentParser.Create<CLIArguments>(programName =  (System.Reflection.Assembly.GetExecutingAssembly().GetName().Name))
         let results = parser.Parse argv
+        let quant = results.GetResult QuantFile
+        let prot  = results.GetResult ProtFile
+        let o     = results.GetResult OutputDirectory
+        let p     = results.GetResult ParamFile
+        Logging.generateConfig o
+        let logger = Logging.createLogger "SpectralLibrary"
+        logger.Info (sprintf "QuantFilePath -i = %s" quant)
+        logger.Info (sprintf "ProtFilePath = %s" prot)
+        logger.Info (sprintf "OutputFolderPath -o = %s" o)
+        logger.Info (sprintf "InputParameterPath -p = %s" p)
+        logger.Trace (sprintf "CLIArguments: %A" results)
+        Directory.CreateDirectory(o) |> ignore
+        if Directory.Exists quant && Directory.Exists prot then
+            logger.Info (sprintf "multiple files")
+            let quantFiles =
+                Directory.GetFiles(quant,("*.quant"))
+            let protFiles =
+                Directory.GetFiles(prot,("*.prot"))
+            let matchedFiles =
+                quantFiles
+                |> Array.collect (fun quantFile ->
+                    protFiles
+                    |> Array.choose (fun protFile ->
+                        if Path.GetFileNameWithoutExtension quantFile = Path.GetFileNameWithoutExtension protFile then
+                            Some (quantFile, protFile)
+                        else
+                            None
+                    )
+                )
+            matchedFiles
+            |> Array.unzip
+            |> fun (quant, prot) -> sortTables quant prot o
+            |> ignore
+        elif (File.Exists quant) || (File.Exists prot) then
+            failwith "Both, quant file path and prot file path, must be a folder."
+        else
+            failwith "The given paths are no valid folder paths"
         printfn "Hit any key to exit."
         System.Console.ReadKey() |> ignore
         0
