1c95fb69d245f25a67088198c15fb61534c408a7
David Zimmer <davidzimmer91@gmail.com>
2020-07-20 18:59:49 +0200

    first draft

--- src/AlignmentBasedQuantification/Program.fs
+++ src/AlignmentBasedQuantification/Program.fs
@@ -1,17 +1,78 @@
 namespace ProteomIQon
 
-open System
+open System.IO
 open CLIArgumentParsing
 open Argu
+open AlignmentBasedQuantification
+
 module console1 =
+    open BioFSharp.Mz
 
     [<EntryPoint>]
-    let main argv = 
+    let main argv =
         printfn "%A" argv
 
         let parser = ArgumentParser.Create<CLIArguments>(programName =  (System.Reflection.Assembly.GetExecutingAssembly().GetName().Name))
         let results = parser.Parse argv
-        Library.printParams (results.GetAllResults())
-        printfn "Hit any key to exit."
-        System.Console.ReadKey() |> ignore
+        let i = results.GetResult InstrumentOutput
+        let ii = results.GetResult AlignedPeptides
+        let o = results.GetResult OutputDirectory
+        let p = results.GetResult ParamFile
+        let d = results.GetResult PeptideDataBase
+        Logging.generateConfig o
+        let logger = Logging.createLogger "AlignmentBasedQuantification"
+        logger.Info (sprintf "InputFilePath -i = %s" i)
+        logger.Info (sprintf "AlignedPeptides -ii = %s" ii)
+        logger.Info (sprintf "OutputFilePath -o = %s" o)
+        logger.Info (sprintf "ParamFilePath -p = %s" p)
+        logger.Trace (sprintf "CLIArguments: %A" results)
+        Directory.CreateDirectory(o) |> ignore
+        let p =
+            Json.ReadAndDeserialize<Dto.QuantificationParams> p
+            |> Dto.QuantificationParams.toDomain
+        let dbConnection =
+            if File.Exists d then
+                logger.Trace (sprintf "Database found at given location (%s)" d)
+                SearchDB.getDBConnection d
+            else
+                failwith "The given path to the instrument output is neither a valid file path nor a valid directory path."
+        logger.Trace "Set Index on data base if not present."
+        SearchDB'.setIndexOnModSequenceAndGlobalMod dbConnection |> ignore
+        logger.Trace "Set Index on data base if not present: finished"
+        if File.Exists i then
+            logger.Info "single file"
+            quantifyPeptides p o dbConnection i ii
+        elif Directory.Exists i && Directory.Exists ii then
+            logger.Info "multiple files"
+            let mzfiles =
+                Directory.GetFiles(i,("*.mzlite"))
+            let pepfiles =
+                Directory.GetFiles(ii,("*.align"))
+            logger.Trace (sprintf "mz files : %A" mzfiles)
+            logger.Trace (sprintf "align files : %A" pepfiles)
+            let mzFilesAndPepFiles =
+                mzfiles
+                |> Array.choose (fun mzFilePath ->
+                    match pepfiles  |> Array.tryFind (fun pepFilePath -> (Path.GetFileNameWithoutExtension pepFilePath) = (Path.GetFileNameWithoutExtension mzFilePath)) with
+                    | Some pepFile -> Some(mzFilePath,pepFile)
+                    | None ->
+                        logger.Trace (sprintf "no qpsmFileFor %s" mzFilePath)
+                        None
+                    )
+            if mzfiles.Length <> mzFilesAndPepFiles.Length then
+                logger.Info (sprintf "There are %i mzFiles but %i files containing scored PSMs" mzfiles.Length pepfiles.Length)
+            let c =
+                match results.TryGetResult Parallelism_Level with
+                | Some c    -> c
+                | None      -> 1
+            logger.Trace (sprintf "Program is running on %i cores" c)
+            mzFilesAndPepFiles
+            |> FSharpAux.PSeq.map (fun (i,ii) -> quantifyPeptides p o dbConnection i ii)
+            |> FSharpAux.PSeq.withDegreeOfParallelism c
+            |> Array.ofSeq
+            |> ignore
+        else
+            failwith "The given path to the instrument output is neither a valid file path nor a valid directory path."
+
+        logger.Info "Done"
         0
