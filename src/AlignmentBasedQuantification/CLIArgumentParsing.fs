4e3354ec78d05cc314d18f7d6e413bde6eed76df
David Zimmer <davidzimmer91@gmail.com>
2020-07-21 13:26:35 +0200

    add reference to param type; add Metric file to input files

--- src/AlignmentBasedQuantification/CLIArgumentParsing.fs
+++ src/AlignmentBasedQuantification/CLIArgumentParsing.fs
@@ -9,6 +9,7 @@ module CLIArgumentParsing =
     type CLIArguments =
         | [<Mandatory>] [<AltCommandLine("-i")>]  InstrumentOutput of path:string
         | [<Mandatory>] [<AltCommandLine("-ii")>] AlignedPeptides of path:string
+        | [<Mandatory>] [<AltCommandLine("-iii")>] Metrics of path:string
         | [<Mandatory>] [<AltCommandLine("-d")>]  PeptideDataBase of path:string 
         | [<Mandatory>] [<AltCommandLine("-o")>]  OutputDirectory  of path:string 
         | [<Mandatory>] [<AltCommandLine("-p")>]  ParamFile of path:string
@@ -21,6 +22,7 @@ module CLIArgumentParsing =
                 match s with
                 | InstrumentOutput _    -> "Specify the mass spectrometry output, either specify a file directory containing the files to be analyzed or specify the file path of a single mzlite file."
                 | AlignedPeptides  _    -> "Specify the aligned peptide spectrum matches, either specify a file directory containing the files to be analyzed or specify the file path of a single qpsm file. If InstrumentOutPut(i) and ScoredPSMs (ii) both reference a directory the files are automatically aligned by their file name."
+                | Metrics  _            -> "Specify the .alignmetrics file used to assess false positive alignments, either specify a file directory containing the files to be analyzed or specify the file path of a single qpsm file. If InstrumentOutPut(i) and ScoredPSMs (ii) both reference a directory the files are automatically aligned by their file name."
                 | PeptideDataBase  _    -> "Specify the file path of the peptide data base."
                 | OutputDirectory  _    -> "Specify the output directory."
                 | ParamFile _           -> "Specify parameter file for peptide spectrum matching."
