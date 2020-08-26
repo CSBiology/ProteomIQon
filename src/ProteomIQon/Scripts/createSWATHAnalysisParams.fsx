a130981e0a3f3c5a0d8b7f0faf3fbbf1ae381399
caroott <caroline.ott1994@gmail.com>
2020-08-26 12:52:20 +0200

    Change peak finding to wavelet

--- src/ProteomIQon/Scripts/createSWATHAnalysisParams.fsx
+++ src/ProteomIQon/Scripts/createSWATHAnalysisParams.fsx
@@ -2,14 +2,34 @@
 // for more guidance on F# programming.
 #r "netstandard"
 #r @"../../../bin\ProteomIQon\netstandard2.0\ProteomIQon.dll"
+#r @"../../../packages\FSharp.Stats\lib\netstandard2.0\FSharp.Stats.dll"
 
 open ProteomIQon
 open ProteomIQon.Dto
+open ProteomIQon.FSharpStats'.Wavelet
+open FSharp.Stats.Signal
+open ProteomIQon.Domain
+
+let waveletParams :WaveletParameters = 
+    {
+        Borderpadding           = None    
+        BorderPadMethod         = Padding.BorderPaddingMethod.Random 
+        InternalPaddingMethod   = Padding.InternalPaddingMethod.LinearInterpolation 
+        HugeGapPaddingMethod    = Padding.HugeGapPaddingMethod.Zero
+        HugeGapPaddingDistance  = 100.
+        MinPeakDistance         = None
+        MinPeakLength           = Some 0.1
+        MaxPeakLength           = 1.5 
+        NoiseQuantile           = 0.01 
+        MinSNR                  = 0.01  
+    }
 
 let defaultSWATHAnalysisParams: Dto.SWATHAnalysisParams =
     {
         PeptideList          = None
         MatchingTolerancePPM = 100.
+        QueryOffsetRange     = 10.
+        XicProcessing        = Wavelet waveletParams
     }
 
 let serialized = 
