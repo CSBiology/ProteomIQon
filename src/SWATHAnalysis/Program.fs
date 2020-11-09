9d116c8f33b6a0619d2e3b7224bc88bd26e2714c
David Zimmer <davidzimmer91@gmail.com>
2020-11-09 02:27:47 +0100

    first draft of quantification result

--- src/SWATHAnalysis/Program.fs
+++ src/SWATHAnalysis/Program.fs
@@ -3,54 +3,77 @@ namespace ProteomIQon
 open System.IO
 open CLIArgumentParsing
 open Argu
-open SWATHAnalysis
+open SwathAnalysis
+
 module console1 =
+    open BioFSharp.Mz
 
     [<EntryPoint>]
-    let main argv = 
+    let main argv =
         printfn "%A" argv
 
         let parser = ArgumentParser.Create<CLIArguments>(programName =  (System.Reflection.Assembly.GetExecutingAssembly().GetName().Name))
         let results = parser.Parse argv
-        let i = results.GetResult InstrumentOutput 
+        let i = results.GetResult InstrumentOutput
+        let ii = results.GetResult ConsensusLibary
         let o = results.GetResult OutputDirectory
         let p = results.GetResult ParamFile
-        let l = results.GetResult Library
+        let d = results.GetResult PeptideDataBase
         Logging.generateConfig o
-        let logger = Logging.createLogger "SWATHAnalysis"
+        let logger = Logging.createLogger "SwathAnalysis"
         logger.Info (sprintf "InputFilePath -i = %s" i)
+        logger.Info (sprintf "ConsensusLibraryPath -ii = %s" ii)
         logger.Info (sprintf "OutputFilePath -o = %s" o)
         logger.Info (sprintf "ParamFilePath -p = %s" p)
-        logger.Info (sprintf "LibraryFilePath -l = %s" l)
         logger.Trace (sprintf "CLIArguments: %A" results)
         Directory.CreateDirectory(o) |> ignore
         let p =
-            Json.ReadAndDeserialize<Dto.SWATHAnalysisParams> p
-            |> Dto.SWATHAnalysisParams.toDomain
+            Json.ReadAndDeserialize<Dto.ConsensusSpectralLibraryParams> p
+            |> Dto.ConsensusSpectralLibraryParams.toDomain
+        let dbConnection =
+            if File.Exists d then
+                logger.Trace (sprintf "Database found at given location (%s)" d)
+                SearchDB.getDBConnection d
+            else
+                failwith "The given path to the instrument output is neither a valid file path nor a valid directory path."
+        logger.Trace "Set Index on data base if not present."
+        SearchDB'.setIndexOnModSequenceAndGlobalMod dbConnection |> ignore
+        logger.Trace "Set Index on data base if not present: finished"
         if File.Exists i then
             logger.Info "single file"
-            quantify l p i o
-        elif Directory.Exists i then
+            quantify p o (*dbConnection*) i ii
+        elif Directory.Exists i && Directory.Exists ii then
             logger.Info "multiple files"
-            let files = 
+            let mzfiles =
                 Directory.GetFiles(i,("*.mzlite"))
-            logger.Trace (sprintf "Quantifying multiple swath files: %A" files)
+            let pepfiles =
+                Directory.GetFiles(ii,("*.csl"))
+            logger.Trace (sprintf "swath files : %A" mzfiles)
+            logger.Trace (sprintf "libraryfiles files : %A" pepfiles)
+            let mzFilesAndPepFiles =
+                mzfiles
+                |> Array.choose (fun mzFilePath ->
+                                match pepfiles  |> Array.tryFind (fun pepFilePath -> (Path.GetFileNameWithoutExtension pepFilePath) = (Path.GetFileNameWithoutExtension mzFilePath)) with
+                                | Some pepFile -> Some(mzFilePath,pepFile)
+                                | None ->
+                                    logger.Trace (sprintf "no qpsmFileFor %s" mzFilePath)
+                                    None
+
+                              )
+            if mzfiles.Length <> mzFilesAndPepFiles.Length then
+                logger.Info (sprintf "There are %i mzFiles but %i files containing scored PSMs" mzfiles.Length pepfiles.Length)
             let c =
                 match results.TryGetResult Parallelism_Level with
                 | Some c    -> c
                 | None      -> 1
             logger.Trace (sprintf "Program is running on %i cores" c)
-            try
-                let partitionedFiles =
-                    files
-                    |> Array.splitInto c
-                [for i in partitionedFiles do yield async { return i |> Array.map (fun file -> (quantify l p file o))}]
-                |> Async.Parallel
-                |> Async.RunSynchronously
-                |> ignore
-            with | ex -> printfn "%A" ex
+            mzFilesAndPepFiles
+            |> FSharpAux.PSeq.map (fun (i,ii) -> quantify p o (*dbConnection*) i ii)
+            |> FSharpAux.PSeq.withDegreeOfParallelism c
+            |> Array.ofSeq
+            |> ignore
         else
             failwith "The given path to the instrument output is neither a valid file path nor a valid directory path."
-        printfn "Hit any key to exit."
-        System.Console.ReadKey() |> ignore
+
+        logger.Info "Done"
         0
