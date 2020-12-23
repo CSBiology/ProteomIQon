7bed615d09382bd56a2fd17060fdc24e23de821a
Jonathan Ott <jonathan.ott1994@gmail.com>
2020-12-23 18:55:38 +0100

    Change stdev and sem for proteins to take info from tablesort

--- src/MzTAB/MzTABFormater.fs
+++ src/MzTAB/MzTABFormater.fs
@@ -226,7 +226,7 @@ module MzTABFormater =
         psmS
         |> Array.iter (fun psm ->
             sb.AppendFormat(
-                "PSM\t{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\t{8}\t{9}\t{10}\t{11}\t{12}\t{13}\t{14}\t{15}\t{16}\t{17}\t{18}\t{19}",
+                "PSM\t{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\t{8}\t{9}\t{10}\t{11}\t{12}\t{13}\t{14}\t{15}\t{16}\t{17}\t{18}\t{19}\t{20}",
                 psm.sequence,
                 psm.PSM_ID,
                 (
