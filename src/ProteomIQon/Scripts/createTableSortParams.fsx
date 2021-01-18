d3ae826f1fcaa13af630dec5f60dfff32d3642a4
caroott <caroline.ott1994@gmail.com>
2021-01-18 12:06:52 +0100

    Update default parameters

--- src/ProteomIQon/Scripts/createTableSortParams.fsx
+++ src/ProteomIQon/Scripts/createTableSortParams.fsx
@@ -20,13 +20,13 @@ let defaultTableSortParams: Dto.TableSortParams =
         // proteinIDs = Name of the column containing the protein names in the prot file
         // pepSequence = Name of the column containing the peptide sequence in the quant file
         // pepSequences = Name of the column containing the peptide sequences in the prot file
-        EssentialFields             = EssentialFields.create "Quant_Light" (Some "Quant_Heavy") "GroupOfProteinIDs" "StringSequence" "PeptideSequence"
-        QuantFieldsToFilterOn       = [|(FilterOnField.create "Quant_Light" (None) (Some 0.)); (FilterOnField.create "Quant_Heavy" (None) (Some 0.))|]
+        EssentialFields             = EssentialFields.create "MeasuredApex_Light" (Some "MeasuredApex_Heavy") "GroupOfProteinIDs" "StringSequence" "PeptideSequence"
+        QuantFieldsToFilterOn       = [|(FilterOnField.create "MeasuredApex_Light" (None) (Some 0.)); (FilterOnField.create "MeasuredApex_Heavy" (None) (Some 0.))|]
         ProtFieldsToFilterOn        = [|(*(FilterOnField.create "Class" None (Some 1.))*)|]
         // Columns of interest can be any column present in the original tables. The column "Ratio" is automatically added
         // when heavy labeled peptides are present. "DistinctPeptideCount" is calculated from the amount of different peptides used
         // for the ratio calculation and can be included.
-        QuantColumnsOfInterest      = [|"Quant_Light";"Quant_Heavy"|]
+        QuantColumnsOfInterest      = [|"MeasuredApex_Light";"MeasuredApex_Heavy"|]
         ProtColumnsOfInterest       = [|"QValue", "float"|]
         DistinctPeptideCount        = true
         StatisticalMeasurements     = [|("Ratio", StatisticalMeasurement.CV); ("Ratio", StatisticalMeasurement.SEM); ("Ratio", StatisticalMeasurement.StDev)|]
@@ -40,7 +40,7 @@ let serialized =
     defaultTableSortParams
     |> Json.serialize
 
-System.IO.File.WriteAllText(__SOURCE_DIRECTORY__ + @"/../defaultParams\TableSortParams.json",serialized)
+System.IO.File.WriteAllText(__SOURCE_DIRECTORY__ + @"/../defaultParams\TableSortParamsLabeled.json",serialized)
 
 let deserialized = 
     System.IO.File.ReadAllText(__SOURCE_DIRECTORY__ + @"/../defaultParams\TableSortParams.json")
