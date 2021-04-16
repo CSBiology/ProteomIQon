0914a2234ecf35f6ddfb89dc8b618f06a66d96cc
caroott <caroline.ott1994@gmail.com>
2021-04-16 20:15:24 +0200

    Relocate function to get files from inputlists to Core

--- src/ProteinInference/Program.fs
+++ src/ProteinInference/Program.fs
@@ -6,6 +6,7 @@ open Argu
 open BioFSharp.Mz
 open System.Reflection
 open FSharpAux
+open ProteomIQon.Core.InputPaths
 
 module console1 =
 
@@ -15,11 +16,11 @@ module console1 =
 
         let parser = ArgumentParser.Create<CLIArguments>(programName =  (System.Reflection.Assembly.GetExecutingAssembly().GetName().Name)) 
         let results = parser.Parse argv
-        let i'     = results.GetResult InputFolder
-        let d'     = results.GetResult PeptideDataBase
-        let gff3'  = results.TryGetResult GFF3
-        let o'     = results.GetResult OutputDirectory
-        let p'     = results.GetResult ParamFile
+        let i'    = results.GetResult InputFolder
+        let d'    = results.GetResult PeptideDataBase
+        let gff3' = results.TryGetResult GFF3
+        let o'    = results.GetResult OutputDirectory
+        let p'    = results.GetResult ParamFile
         let directory = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)
         let d = Path.Combine(directory, d')
         let gff3 =
@@ -49,36 +50,7 @@ module console1 =
         let proteinInferenceParams = 
                 Json.ReadAndDeserialize<Dto.ProteinInferenceParams> p
                 |> Dto.ProteinInferenceParams.toDomain
-        if i'.Contains(";") then
-            let stringList =
-                i'
-                // Replace and Trim not needed?
-                |> String.replace "\"" ""
-                |> String.split ';'
-                |> Array.map (fun x -> x.Trim())
-            let pathList =
-                stringList
-                |> Array.map (fun path -> Path.Combine(directory, path))
-            let fileList =
-                pathList
-                |> Array.collect (fun path ->
-                    if Directory.Exists path then
-                        Directory.GetFiles(path,("*.qpsm"))
-                    elif File.Exists path then
-                        [|path|]
-                    else
-                        failwith "The given path to the PSMs is neither a valid file path nor a valid directory path."
-                )
-            ProteinInference.inferProteins gff3 dbConnection proteinInferenceParams o fileList
-        elif File.Exists (Path.Combine(directory, i')) then
-            logger.Info (sprintf "single file")
-            ProteinInference.inferProteins gff3 dbConnection proteinInferenceParams o [|Path.Combine(directory, i')|]
-        elif Directory.Exists (Path.Combine(directory, i')) then
-            logger.Info (sprintf "multiple files")
-            let files = 
-                Directory.GetFiles(Path.Combine(directory, i'),("*.qpsm"))
-            ProteinInference.inferProteins gff3 dbConnection proteinInferenceParams o files
-        else 
-            failwith "The given path to the PSMs is neither a valid file path nor a valid directory path."
+        let files = getFileArray directory i' "qpsm"
+        ProteinInference.inferProteins gff3 dbConnection proteinInferenceParams o files
         logger.Info "Done"
         0
