9a17317ba49752529e9e23f40693973c19c1d304
David Zimmer <davidzimmer91@gmail.com>
2021-04-20 13:44:53 +0200

    change handling of --help

--- src/PeptideDB/Program.fs
+++ src/PeptideDB/Program.fs
@@ -10,30 +10,22 @@ module console1 =
 
     [<EntryPoint>]
     let main argv =
-        printfn "%A" argv
-
-        let parser = ArgumentParser.Create<CLIArguments>(programName =  (System.Reflection.Assembly.GetExecutingAssembly().GetName().Name))
+        let errorHandler = ProcessExiter(colorizer = function ErrorCode.HelpText -> None | _ -> Some System.ConsoleColor.Red)
+        let parser = ArgumentParser.Create<CLIArguments>(programName =  (System.Reflection.Assembly.GetExecutingAssembly().GetName().Name),errorHandler=errorHandler)     
         let directory = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)
         let getPathRelativeToDir = getRelativePath directory
         let results = parser.Parse argv
-        let inputFasta = results.TryGetResult FastaPath
-        let outputDir = results.TryGetResult OutputDirectory
-        let paramF = results.TryGetResult ParamFile
-        match inputFasta, outputDir, paramF with
-        | Some i', Some o' , Some p' ->
-            let i = i' |> getPathRelativeToDir
-            let o = o' |> getPathRelativeToDir
-            let p = p' |> getPathRelativeToDir
-            Logging.generateConfig outputDir.Value
-            let logger = Logging.createLogger "PeptideDB"
-            logger.Trace (sprintf "CLIArguments %A" results)
-            logger.Info (sprintf "OutputFilePath %s" o)
-            logger.Info (sprintf "ParamFilePath %s" p)
-            Directory.CreateDirectory(o) |> ignore
-            let processParams =
-                Json.ReadAndDeserialize<Dto.PeptideDBParams> p
-                |> Dto.PeptideDBParams.toDomain
-            
-            PeptideDB.createPeptideDB processParams o i
-        | _ -> failwith "parameterfile or output directory have no valid path or are no valid file"
+        let i = results.GetResult FastaPath |> getPathRelativeToDir
+        let o = results.GetResult OutputDirectory |> getPathRelativeToDir
+        let p = results.GetResult ParamFile |> getPathRelativeToDir
+        Logging.generateConfig o
+        let logger = Logging.createLogger "PeptideDB"
+        logger.Trace (sprintf "CLIArguments %A" results)
+        logger.Info (sprintf "OutputFilePath %s" o)
+        logger.Info (sprintf "ParamFilePath %s" p)
+        Directory.CreateDirectory(o) |> ignore
+        let processParams =
+            Json.ReadAndDeserialize<Dto.PeptideDBParams> p
+            |> Dto.PeptideDBParams.toDomain           
+        PeptideDB.createPeptideDB processParams o i
         0
