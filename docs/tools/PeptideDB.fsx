8ded07a24f73f5ce7a777196d6d15dbaa3913be2
Jonathan Ott <jonathan.ott1994@gmail.com>
2021-04-21 15:33:24 +0200

    Add download links

--- docs/tools/PeptideDB.fsx
+++ docs/tools/PeptideDB.fsx
@@ -5,6 +5,11 @@ category: Tools
 categoryindex: 1
 index: 1
 ---
+
+[![Binder]({{root}}img/badge-binder.svg)](https://mybinder.org/v2/gh/csbiology/BIO-BTE-12-V-4/gh-pages?filepath={{fsdocs-source-basename}}.ipynb)&emsp;
+[![Script]({{root}}img/badge-script.svg)]({{root}}{{fsdocs-source-basename}}.fsx)&emsp;
+[![Notebook]({{root}}img/badge-notebook.svg)]({{root}}{{fsdocs-source-basename}}.ipynb)
+
 # PeptideDB
 
 This tool takes proteome information in the form of a [FASTA](https://en.wikipedia.org/wiki/FASTA_format) file. The proteome information is then processed according 
@@ -29,7 +34,7 @@ to the parameters given and stored in a database. The database contains informat
 | VarModThreshold            | 4                                                                 | Threshold for variable modifications                                |
 
 Parameters are handed to the cli tool as a .json file. you can download the default file [here](https://github.com/CSBiology/ProteomIQon/blob/master/src/ProteomIQon/defaultParams/peptideDBParams.json), 
-or use an F# script to write your own parameter file:
+or use an F# script, which can be downloaded or run in Binder at the top of the page, to write your own parameter file:
 *)
 
 #r "nuget: BioFSharp.Mz, 0.1.5-beta"
