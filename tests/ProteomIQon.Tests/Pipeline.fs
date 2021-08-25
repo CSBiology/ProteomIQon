80681abe703b45d89eb8fadd653e71e546d1dd9f
Jonathan Ott <jonathan.ott1994@gmail.com>
2021-08-25 16:14:21 +0200

    Start adding pipeline tests

---
+++ tests/ProteomIQon.Tests/Pipeline.fs
@@ -0,0 +1,30 @@
+﻿module Pipeline
+
+open Expecto
+open System
+open System.IO
+
+[<Tests>]
+let pipelineTests =
+    testList "Pipeline" [
+        testCase "PeptideDB" <| fun _ ->
+            let directory = Environment.CurrentDirectory
+            printfn "%s" directory
+            Expect.isTrue true "I compute, therefore I am."
+    
+        testCase "PeptideSpectrumMatching" <| fun _ ->
+            let subject = true
+            Expect.isTrue subject "I compute, therefore I am."
+
+        testCase "PSMStatistics" <| fun _ ->
+            let subject = true
+            Expect.isTrue subject "I compute, therefore I am."
+
+        testCase "PSMBasedQuantification" <| fun _ ->
+            let subject = true
+            Expect.isTrue subject "I compute, therefore I am."
+
+        testCase "ProteinInference" <| fun _ ->
+            let subject = true
+            Expect.isTrue subject "I compute, therefore I am."
+    ]
