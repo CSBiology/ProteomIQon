7959ce665f5b1db0400b28a89a0eddff8602072f
caroott <caroline.ott1994@gmail.com>
2021-09-15 15:15:23 +0200

    Add tool description placeholders

---
+++ docs/tools/JoinQuantPepIonsWithProteins.fsx
@@ -0,0 +1,16 @@
+(**
+---
+title: JoinQuantPepIonsWithProteins
+category: Tools
+categoryindex: 1
+index: 8
+---
+*)
+
+(**
+[![Binder]({{root}}img/badge-binder.svg)](https://mybinder.org/v2/gh/csbiology/ProteomIQon/gh-pages?filepath={{fsdocs-source-basename}}.ipynb)&emsp;
+[![Script]({{root}}img/badge-script.svg)]({{root}}{{fsdocs-source-basename}}.fsx)&emsp;
+[![Notebook]({{root}}img/badge-notebook.svg)]({{root}}{{fsdocs-source-basename}}.ipynb)
+
+# Join Quant Peptide-Ions With Proteins
+*)
