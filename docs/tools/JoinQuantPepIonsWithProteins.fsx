3ee1600a7eafbafdf08187d32d8b84815c52cecc
Joott <jonathan.ott1994@gmail.com>
2021-09-15 16:38:03 +0200

    Add JoinQuantPepIonsWithProteins description

--- docs/tools/JoinQuantPepIonsWithProteins.fsx
+++ docs/tools/JoinQuantPepIonsWithProteins.fsx
@@ -13,4 +13,10 @@ index: 8
 [![Notebook]({{root}}img/badge-notebook.svg)]({{root}}{{fsdocs-source-basename}}.ipynb)
 
 # Join Quant Peptide-Ions With Proteins
+**Disclaimer** this tool needs the results from [ProteinInference]({{root}}tools/ProteinInference.html) and [PSMBasedQuantification]({{root}}tools/PSMBasedQuantification.html).
+
+Results from [PSMBasedQuantification]({{root}}tools/PSMBasedQuantification.html) contain detailed information about the quantification of every peptide, but only basic informations about the protein they originated from. 
+This information is obtained during the [ProteinInference]({{root}}tools/ProteinInference.html). This tool takes the results from both tools and combines the qauntification information of each peptide with the more accurate information about 
+the corresponding protein including its q-value.
+
 *)
