8e0feb5546bc9df9d9a88555103c88829880e317
Joott <39764934+Joott@users.noreply.github.com>
2021-09-15 14:39:58 +0000

    Deploying to gh-pages from  @ 3ee1600a7eafbafdf08187d32d8b84815c52cecc 🚀

--- tools/JoinQuantPepIonsWithProteins.fsx
+++ tools/JoinQuantPepIonsWithProteins.fsx
@@ -6,5 +6,11 @@
 [![Notebook](https://csbiology.github.io/ProteomIQon/img/badge-notebook.svg)](https://csbiology.github.io/ProteomIQon/tools/JoinQuantPepIonsWithProteins.ipynb)
 
 # Join Quant Peptide-Ions With Proteins
+**Disclaimer** this tool needs the results from [ProteinInference](https://csbiology.github.io/ProteomIQon/tools/ProteinInference.html) and [PSMBasedQuantification](https://csbiology.github.io/ProteomIQon/tools/PSMBasedQuantification.html).
+
+Results from [PSMBasedQuantification](https://csbiology.github.io/ProteomIQon/tools/PSMBasedQuantification.html) contain detailed information about the quantification of every peptide, but only basic informations about the protein they originated from. 
+This information is obtained during the [ProteinInference](https://csbiology.github.io/ProteomIQon/tools/ProteinInference.html). This tool takes the results from both tools and combines the qauntification information of each peptide with the more accurate information about 
+the corresponding protein including its q-value.
+
 
 *)
