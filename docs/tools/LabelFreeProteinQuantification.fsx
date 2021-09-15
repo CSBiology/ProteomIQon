7e8ff3123060460bab6ab9c318e77f5f6268b8b7
Joott <jonathan.ott1994@gmail.com>
2021-09-15 15:15:23 +0200

    Add tool description placeholders

---
+++ docs/tools/LabelFreeProteinQuantification.fsx
@@ -0,0 +1,16 @@
+(**
+---
+title: LabelFreeProteinQuantification
+category: Tools
+categoryindex: 1
+index: 9
+---
+*)
+
+(**
+[![Binder]({{root}}img/badge-binder.svg)](https://mybinder.org/v2/gh/csbiology/ProteomIQon/gh-pages?filepath={{fsdocs-source-basename}}.ipynb)&emsp;
+[![Script]({{root}}img/badge-script.svg)]({{root}}{{fsdocs-source-basename}}.fsx)&emsp;
+[![Notebook]({{root}}img/badge-notebook.svg)]({{root}}{{fsdocs-source-basename}}.ipynb)
+
+# Labelfree Protein Quantification
+*)
---
+++ docs/tools/LabeledProteinQuantification.fsx
@@ -0,0 +1,16 @@
+(**
+---
+title: LabeledProteinQuantification
+category: Tools
+categoryindex: 1
+index: 10
+---
+*)
+
+(**
+[![Binder]({{root}}img/badge-binder.svg)](https://mybinder.org/v2/gh/csbiology/ProteomIQon/gh-pages?filepath={{fsdocs-source-basename}}.ipynb)&emsp;
+[![Script]({{root}}img/badge-script.svg)]({{root}}{{fsdocs-source-basename}}.fsx)&emsp;
+[![Notebook]({{root}}img/badge-notebook.svg)]({{root}}{{fsdocs-source-basename}}.ipynb)
+
+# Labeled Protein Quantification
+*)
