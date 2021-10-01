39af45f0e9bbb641422c35776845c6eb05ff371e
caroott <caroline.ott1994@gmail.com>
2021-10-01 20:26:13 +0200

    Add template for AddDeducedPeptides

---
+++ src/AddDeducedPeptides/CLIArgumentParsing.fs
@@ -0,0 +1,27 @@
+namespace ProteomIQon
+
+open System.IO
+open Argu
+
+module CLIArgumentParsing = 
+
+    type CLIArguments =
+        | [<Mandatory>] [<AltCommandLine("-i")>] InstrumentOutput of path:string list
+        | [<Mandatory>] [<AltCommandLine("-o")>] OutputDirectory  of path:string 
+        | [<Mandatory>] [<AltCommandLine("-p")>] ParamFile of path:string
+    with
+        interface IArgParserTemplate with
+            member s.Usage =
+                match s with
+                | InstrumentOutput _    -> "Specify the mass spectrometry output, either specify a file directory containing the files to be analyzed or specify the file path of a single mzlite file."
+                | OutputDirectory  _    -> "Specify the output directory."
+                | ParamFile _           -> "Specify parameter file for peptide spectrum matching."
+
+
+
+
+
+
+
+
+
