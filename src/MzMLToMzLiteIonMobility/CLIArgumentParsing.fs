6025f90b9435e226f413b9e840c368cc30bec1ad
Jonathan Ott <jonathan.ott1994@gmail.com>
2023-06-13 15:01:07 +0200

    Add conversion code and program logic

--- src/MzMLToMzLiteIonMobility/CLIArgumentParsing.fs
+++ src/MzMLToMzLiteIonMobility/CLIArgumentParsing.fs
@@ -1,27 +1,24 @@
 namespace ProteomIQon
 
-open System.IO
 open Argu
 
 module CLIArgumentParsing = 
-
+    open System.IO
+  
     type CLIArguments =
-        | [<Mandatory>] [<AltCommandLine("-i")>] InstrumentOutput of path:string list
-        | [<Mandatory>] [<AltCommandLine("-o")>] OutputDirectory  of path:string 
-        | [<Mandatory>] [<AltCommandLine("-p")>] ParamFile of path:string
+        | [<AltCommandLine("-i")>] InstrumentOutput of path:string list
+        | [<AltCommandLine("-o")>] OutputDirectory  of path:string 
+        | [<AltCommandLine("-p")>] ParamFile of path:string
+        | [<Unique>] [<AltCommandLine("-c")>] Parallelism_Level of level:int
+        | [<AltCommandLine("-l")>] Log_Level of level:int
+        | [<AltCommandLine("-v")>] Verbosity_Level of level:int
     with
         interface IArgParserTemplate with
             member s.Usage =
                 match s with
-                | InstrumentOutput _    -> "Specify the mass spectrometry output, either specify a file directory containing the files to be analyzed or specify the file path of a single mzlite file."
-                | OutputDirectory  _    -> "Specify the output directory."
-                | ParamFile _           -> "Specify parameter file for peptide spectrum matching."
-
-
-
-
-
-
-
-
-
+                | InstrumentOutput _ -> "specify mass spectrometry Output"
+                | OutputDirectory  _ -> "specify output directory"
+                | ParamFile _        -> "specify param file For centroidization"
+                | Log_Level _        -> "set the log level."
+                | Verbosity_Level _  -> "set the verbosity level."
+                | Parallelism_Level _-> "Set the number of cores the programm can use. Parallelization occurs on file level. This flag is only of effect if a input directory (-i) is specified."
--- src/MzMLToMzLiteIonMobility/Program.fs
+++ src/MzMLToMzLiteIonMobility/Program.fs
@@ -2,17 +2,16 @@ namespace ProteomIQon
 
 open System
 open System.IO
-open System.Reflection
-open Argu
-open ProteomIQon.Core
-open ProteomIQon.Core.InputPaths
-open Library
 open CLIArgumentParsing
+open Argu
+open System.Reflection
+open ProteomIQon.Core.InputPaths
+open ProteomIQon.Core
 
 module console1 =
 
     [<EntryPoint>]
-    let main argv = 
+    let main argv =
         let errorHandler = ProcessExiter(colorizer = function ErrorCode.HelpText -> None | _ -> Some System.ConsoleColor.Red)
         let parser = ArgumentParser.Create<CLIArguments>(programName =  (System.Reflection.Assembly.GetExecutingAssembly().GetName().Name),errorHandler=errorHandler)     
         let directory = Environment.CurrentDirectory
@@ -23,18 +22,40 @@ module console1 =
         let p = results.GetResult ParamFile        |> getPathRelativeToDir
         Directory.CreateDirectory(o) |> ignore
         Logging.generateConfig o
-        let logger = Logging.createLogger "PeptideSpectrumMatching"
+        let logger = Logging.createLogger "MzMLToMzLite"
         logger.Info (sprintf "InputFilePath -i = %A" i)
         logger.Info (sprintf "OutputFilePath -o = %s" o)
         logger.Info (sprintf "ParamFilePath -p = %s" p)
         logger.Trace (sprintf "CLIArguments: %A" results)
-        let p = 
-            Json.ReadAndDeserialize<Dto.PeptideSpectrumMatchingParams> p
-            |> Dto.PeptideSpectrumMatchingParams.toDomain
+        let processParams =
+            Json.ReadAndDeserialize<Dto.PreprocessingParams> p
+            |> Dto.PreprocessingParams.toDomain
+
         let files = 
-            parsePaths (fun path -> Directory.GetFiles(path,("*.txt"))) i
+            parsePaths (fun path -> MzIO.Reader.getMzMLFiles path) i
             |> Array.ofSeq
-        Library.print (results.GetAllResults())
-        Library.print files     
+
+        if files.Length = 1  then
+            logger.Info "single file"
+            logger.Trace (sprintf "Preprocessing %s" files.[0])
+            MzMLToMzLiteIonMobility.processFile processParams o files.[0]
+        else
+            logger.Info "multiple files"
+            logger.Trace (sprintf "Preprocessing multiple files: %A" files)
+            let c =
+                match results.TryGetResult Parallelism_Level with
+                | Some c    -> c
+                | None      -> 1
+            logger.Trace (sprintf "Program is running on %i cores" c)
+            try
+            let partitionedFiles =
+                files
+                |> Array.splitInto c
+            [for i in partitionedFiles do yield async { return i |> Array.map (MzMLToMzLiteIonMobility.processFile processParams o)}]
+            |> Async.Parallel
+            |> Async.RunSynchronously
+            |> ignore
+            with
+            | ex -> printfn "%A" ex
         logger.Info "Done"
         0
